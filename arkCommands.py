3b5ad7254d85d771ade220c5df9a7a69900fa584
Martin Weber Nissle <webernissle@gmail.com>
2020-04-13 20:36:58 +0200

    added BUFFER handling in systemd for python

--- arkCommands.py
+++ arkCommands.py
@@ -1,15 +1,29 @@
+# -------------------------------------------------------------------------------
+#  Copyright 2018 yafra.org
 #
-import time
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+# -------------------------------------------------------------------------------
+# command module for ark server management called by a bot
+
 import re
 import subprocess
 
 
 async def arkmanager(command: []) -> str:
     result = subprocess.run(command, stdout=subprocess.PIPE,
-                            universal_newlines=True)
+                            universal_newlines=True, shell=False)
     if result.returncode != 0:
         return f"error from system command - error code {result.returncode}"
-    time.sleep(3)
     ansi_escape = re.compile(r'\x1B\[[0-?]*[ -/]*[@-~]')
     converted_result = ansi_escape.sub('', str(result.stdout))
     return converted_result
--- demo.py
+++ demo.py
@@ -1,3 +1,20 @@
+# -------------------------------------------------------------------------------
+#  Copyright 2018 yafra.org
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+# -------------------------------------------------------------------------------
+# simple test / demo function for arkCommands
+
 import asyncio
 import arkCommands
 
@@ -11,4 +28,8 @@ async def main():
     print(ret)
     print(f"finished demo function")
 
-asyncio.run(main())
+
+tst = [main()]
+loop = asyncio.get_event_loop()
+loop.run_until_complete(asyncio.wait(tst))
+#python3.7 asyncio.run(main())
